93a03ffd762b0952474d2886a6cf509d4145a7b8
server <server_91@bk.ru>
2020-11-16 17:49:10 +0300

    js

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+var name = prompt  ('введите ваще имя ');
+var year = prompt  ('сколько вам лет ');
+var adds = +prompt  (' Решите пример 15+12 = ?'  );
+var subtract = +prompt ('Решите пример 20-12 = ?');
+var multiplication = +prompt ('Решите пример  30*2 = ?');
+var division = +prompt ('Решите пример  20/10 = ?');
+var remains = +prompt ('Решите пример  20%3 = ?');
+var text = alert ('Откройте console для просмотра ');
+
+console.log('Меня зовут ' +''+ name);
+console.log('Мне ' + year);
+console.log('Пример 1: ' + "15+12=" + (15+12)  + ' Ваш ответ ' + adds);
+console.log('Пример 2: ' + "20-12=" + (20-12)  + ' Ваш ответ ' + subtract);
+console.log('Пример 3: ' + "30*2=" + (30*2)  + ' Ваш ответ ' + multiplication);
+console.log('Пример 4: ' + "20/10=" + (20/10)  + ' Ваш ответ ' + division);
+console.log('Пример 5: ' + "20%3=" + (20%3)  + ' Ваш ответ ' + remains);
+console.log(text);
